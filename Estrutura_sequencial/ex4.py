8a5246190736a700d5392528e6b95f962281ce48
MarcusLehmkuhl <marcusfilipi.ventura@outlook.com>
2024-04-05 18:45:49 -0300

    ex4

---
+++ Estrutura_sequencial/ex4.py
@@ -0,0 +1,8 @@
+prim_bim = int(input("Digite a nota do bimeste 1 "))
+seg_bim = int(input("Digite a nota do bimeste 2 "))
+terc_bim = int(input("Digite a nota do bimeste 3 "))
+quar_bim = int(input("Digite a nota do bimeste 4 "))
+
+som_bim = prim_bim + seg_bim + terc_bim + quar_bim 
+med_bim = som_bim/4
+print("A média foi:", med_bim)
