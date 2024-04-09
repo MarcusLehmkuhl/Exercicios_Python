67ac09728ab2fb1d4c8e1736a861474e481d1f48
MarcusLehmkuhl <marcusfilipi.ventura@outlook.com>
2024-04-08 21:23:41 -0300

    ex7

---
+++ Estrutura_sequencial/ex7.py
@@ -0,0 +1,8 @@
+lado = float(input("Digite o comprimento do lado do quadrado: "))
+
+area_quadrado = lado * lado
+
+dobro_area = 2 * area_quadrado
+
+print("A área do quadrado é:", area_quadrado)
+print("O dobro da área do quadrado é:", dobro_area)
