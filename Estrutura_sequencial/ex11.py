dd4e900f8fc83f2cea248ae46ba5f3573d6a577d
Marcus Filipi Lehmkuhl <116232636+MarcusLehmkuhl@users.noreply.github.com>
2024-04-12 14:11:25 -0300

    Create ex11.py

---
+++ Estrutura_sequencial/ex11.py
@@ -0,0 +1,10 @@
+numero_inteiro1 = int(input("Digite o primeiro número inteiro: "))
+numero_inteiro2 = int(input("Digite o segundo número inteiro: "))
+numero_real = float(input("Digite um número real: "))
+
+resultado1 = (2 * numero_inteiro1) * (numero_inteiro2 / 2)
+resultado2 = (3 * numero_inteiro1) + numero_real
+resultado3 = numero_real ** 3
+print("O produto do dobro do primeiro com metade do segundo é:", resultado1)
+print("A soma do triplo do primeiro com o terceiro é:", resultado2)
+print("O terceiro elevado ao cubo é:", resultado3)
